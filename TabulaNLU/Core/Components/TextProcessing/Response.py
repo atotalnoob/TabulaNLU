ef5b78e4628b7d38f980288701aa3567ca3717a7
atotalnoob <atotalnoob@users.noreply.github.com>
2019-05-10 14:35:40 -0400

    Reoganization and building into  a production scale application

---
+++ TabulaNLU/Core/Components/TextProcessing/Response.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import Core.Components.MachineLearning.Classifiers as Classifiers
+
+def ProcessText(text, project='default', context={}, model_name='', show_details=True):
+
+    
+    f.reset_default_graph()
+    # Build neural network
+    net = tflearn.input_data(shape=[None, len(train_x[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    # Define model and setup tensorboard
+    model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+    import json
+    #load from IO
+
+def response(sentence, userID='123', show_details=False):
+    results = Classifiers.TFClassifier.classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    # check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        # a random response from the intent
+                        return print(random.choice(i['responses']))
+
+            results.pop(0)

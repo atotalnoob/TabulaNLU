3f3f288023d3ee7442bbf78f8dde48dc0d2c3e5d
atotalnoob <atotalnoob@users.noreply.github.com>
2019-05-16 14:17:14 -0400

    Working on building IO framework for save/loading project files #2

--- TabulaNLU/Core/Components/IO/__init__.py
+++ TabulaNLU/Core/Components/IO/__init__.py
@@ -1,11 +1,33 @@
 import pickle
 import tflearn
+import os
+import Core
+from pathlib import Path
 from datetime import datetime
 def SaveTFModel(model, metadata, intents, project='default'):
+    path = get_project_path()
+
     date = datetime.now().strftime('%Y%m%d-%H%M%S')
-    model.save('../../../models/' + project+'/model_' +date + '/model.tflearn')
-    pickle.dump({'words':metadata.words, 'classes':metadata.classes, 'train_x':metadata.train_x, 'train_y':metadata.train_y}, open( '../../../models/'+project+'/model_' +date + '/training_data', 'wb' ))
-    file = open('../../../models/'+project+'model_' + date + 'intents.json', w)
+
+    model_path = os.path.join(path, 'models', project, 'model_' + date)
+
+    #save model
+    model.save(os.path.join(model_path,'/model.tflearn'))
+
+    #save metadata 
+    pickle.dump({'words':metadata.words, 'classes':metadata.classes, 'train_x':metadata.train_x, 'train_y':metadata.train_y}, open( os.path.join(model_path,'/training_data'), 'wb' ))
+
+    #save intents
+    file = open(os.path.join(model_path,'intents.json'), w)
     file.writelines(intents)
     file.close()
-    
+
+def load_tf_model(model, model_name = '', project_name = 'default'):
+   path = get_project_path()
+
+def get_project_path():
+   path = Path(os.path.abspath(Core))
+   root_path = Path(path.parent)
+   return root_path.resolve()
+
+   
--- TabulaNLU/Core/Components/MachineLearning/Classifiers/TFClassifier.py
+++ TabulaNLU/Core/Components/MachineLearning/Classifiers/TFClassifier.py
@@ -13,122 +13,135 @@ import tflearn
 import tensorflow as tf
 import random
 
-def clean_up_sentence(sentence):
-    # tokenize the pattern
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-    return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-def bow(sentence, words, show_details=False):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-
-    return(np.array(bag))
-
-def classify(sentence):
-    #TODO move into configuration
-    ERROR_THRESHOLD = 0.25
-    # generate probabilities from the model
-    results = model.predict([bow(sentence, words)])[0]
-    # filter out predictions below a threshold
-    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append((classes[r[0]], r[1]))
-    # return tuple of intent and probability
-    return return_list
+class TFClassifier(object):
+    def __init__(self, modelName, projectName='default'):
+        tf.reset_default_graph()
+        # Build neural network
+        net = tflearn.input_data(shape=[None, len(train_x[0])])
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+        net = tflearn.regression(net)
+        self.model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 
 
-def TrainTFClassifier(trainingData, project="default"):
-    intents = json.load(trainingData)
+    def classify(self, sentence):
+        #TODO move into configuration
+        ERROR_THRESHOLD = 0.25
+        # generate probabilities from the model
+        results = model.predict([bow(sentence, words)])[0]
+        # filter out predictions below a threshold
+        results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+        # sort by strength of probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+        for r in results:
+            return_list.append((classes[r[0]], r[1]))
+        # return tuple of intent and probability
+        return return_list
 
-#with open('intents.json') as json_data:
-#intents = json.load(json_data)
 
-    words = []
-    classes = []
-    documents = []
-    ignore_words = ['?']
-    # loop through each sentence in our intents patterns
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            # tokenize each word in the sentence
-            w = nltk.word_tokenize(pattern)
-            # add to our words list
-            words.extend(w)
-            # add to documents in our corpus
-            documents.append((w, intent['tag']))
-            # add to our classes list
-            if intent['tag'] not in classes:
-                classes.append(intent['tag'])
+    def TrainTFClassifier(self, training_data, project="default"):
+        intents = json.load(training_data)
 
-    # stem and lower each word and remove duplicates
-    words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
-    words = sorted(list(set(words)))
+    #with open('intents.json') as json_data:
+    #intents = json.load(json_data)
 
-    # remove duplicates
-    classes = sorted(list(set(classes)))
+        words = []
+        classes = []
+        documents = []
+        ignore_words = ['?']
+        # loop through each sentence in our intents patterns
+        for intent in intents['intents']:
+            for pattern in intent['patterns']:
+                # tokenize each word in the sentence
+                w = nltk.word_tokenize(pattern)
+                # add to our words list
+                words.extend(w)
+                # add to documents in our corpus
+                documents.append((w, intent['tag']))
+                # add to our classes list
+                if intent['tag'] not in classes:
+                    classes.append(intent['tag'])
 
-    training = []
-    output = []
-    # create an empty array for our output
-    output_empty = [0] * len(classes)
+        # stem and lower each word and remove duplicates
+        words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+        words = sorted(list(set(words)))
 
-    # training set, bag of words for each sentence
-    for doc in documents:
-        # initialize our bag of words
-        bag = []
-        # list of tokenized words for the pattern
-        pattern_words = doc[0]
+        # remove duplicates
+        classes = sorted(list(set(classes)))
+
+        training = []
+        output = []
+        # create an empty array for our output
+        output_empty = [0] * len(classes)
+
+        # training set, bag of words for each sentence
+        for doc in documents:
+            # initialize our bag of words
+            bag = []
+            # list of tokenized words for the pattern
+            pattern_words = doc[0]
+            # stem each word
+            pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+            # create our bag of words array
+            for w in words:
+                bag.append(1) if w in pattern_words else bag.append(0)
+
+            # output is a '0' for each tag and '1' for current tag
+            output_row = list(output_empty)
+            output_row[classes.index(doc[1])] = 1
+
+            training.append([bag, output_row])
+
+        # shuffle our features and turn into np.array
+        random.shuffle(training)
+        training = np.array(training)
+
+        # create train and test lists
+        train_x = list(training[:,0])
+        train_y = list(training[:,1])
+
+        tf.reset_default_graph()
+        # Build neural network
+        net = tflearn.input_data(shape=[None, len(train_x[0])])
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, 8)
+        net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+        net = tflearn.regression(net)
+
+        # Define model and setup tensorboard
+        model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+        # Start training (apply gradient descent algorithm)
+        model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+
+        metadata = {}
+        metadata.words = words
+        metadata.classes = classes
+        metadata.train_x = train_x
+        metadata.train_y = train_y
+
+        IO.SaveTFModel(model,metadata, project)
+
+    def clean_up_sentence(self, sentence):
+        # tokenize the pattern
+        sentence_words = nltk.word_tokenize(sentence)
         # stem each word
-        pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-        # create our bag of words array
-        for w in words:
-            bag.append(1) if w in pattern_words else bag.append(0)
+        sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+        return sentence_words
 
-        # output is a '0' for each tag and '1' for current tag
-        output_row = list(output_empty)
-        output_row[classes.index(doc[1])] = 1
+    # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+    def bow(self, sentence, words, show_details=False):
+        # tokenize the pattern
+        sentence_words = clean_up_sentence(sentence)
+        # bag of words
+        bag = [0]*len(words)  
+        for s in sentence_words:
+            for i,w in enumerate(words):
+                if w == s: 
+                    bag[i] = 1
+                    if show_details:
+                        print ("found in bag: %s" % w)
 
-        training.append([bag, output_row])
-
-    # shuffle our features and turn into np.array
-    random.shuffle(training)
-    training = np.array(training)
-
-    # create train and test lists
-    train_x = list(training[:,0])
-    train_y = list(training[:,1])
-
-    tf.reset_default_graph()
-    # Build neural network
-    net = tflearn.input_data(shape=[None, len(train_x[0])])
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, 8)
-    net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-    net = tflearn.regression(net)
-
-    # Define model and setup tensorboard
-    model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-    # Start training (apply gradient descent algorithm)
-    model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-
-    metadata = {}
-    metadata.words = words
-    metadata.classes = classes
-    metadata.train_x = train_x
-    metadata.train_y = train_y
-
-    IO.SaveTFModel(model,metadata, project)
+        return(np.array(bag))
